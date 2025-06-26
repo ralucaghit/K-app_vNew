e922ff71e3c589874098badb8032785e240bae9c
Raluca <ralucaghit8@gmail.com>
2025-06-26 21:27:43 +0300

    Modificari 10

--- app/src/main/java/com/example/k_app_v1/MainActivity.kt
+++ app/src/main/java/com/example/k_app_v1/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.k_app_v1
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,13 +15,11 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val navigateButton: Button = findViewById(R.id.navigateButton)
-
-        navigateButton.setOnClickListener{
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }
+        }, 2000)
     }
 
 }
